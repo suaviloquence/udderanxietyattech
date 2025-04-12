c324bb263e528e27b5d5ace7754fe9b823b45877
m <m@mcarr.one>
2025-04-12 01:04:14 -0700

    input mgr

--- game.js
+++ game.js
@@ -8,9 +8,114 @@ document.addEventListener("DOMContentLoaded", () => {
   /** @type HTMLCanvasElement */
   const canvas = document.getElementById("game");
   const ctx = canvas.getContext("2d");
+  const inp = new InputManager(canvas);
 
-  run(new MeowMinigame(), ctx);
+  run(new MeowMinigame(), ctx, inp);
 });
+
+class InputManager {
+  /**
+   * don't change this or i will kill you
+   * @type {number}
+   */
+  mouseX;
+
+  /**
+   * don't change this or i will kill you
+   * @type {number}
+   */
+  mouseY;
+
+  /**
+   * @param {HTMLCanvasElement} canvas
+   */
+  constructor(canvas) {
+    this.down = new Set();
+    this.pressed = new Set();
+    this.up = new Set();
+    this.mouseX = 0;
+    this.mouseY = 0;
+
+    canvas.addEventListener("keydown", (evt) => this.onKeyDown(evt));
+    canvas.addEventListener("keyup", (evt) => this.onKeyUp(evt));
+    canvas.addEventListener("mousedown", (evt) => this.onMouseDown(evt));
+    canvas.addEventListener("mouseup", (evt) => this.onMouseUp(evt));
+    canvas.addEventListener("mousemove", (evt) => this.onMouseMove(evt));
+  }
+
+  isKeyDown(code) {
+    return this.down.has({ key: code });
+  }
+
+  isKeyUp(code) {
+    return this.up.has({ key: code });
+  }
+
+  isKeyPressed(code) {
+    return this.pressed.has({ key: code });
+  }
+
+  isMouseDown(button) {
+    return this.down.has({ mouse: button });
+  }
+
+  isMouseUp(button) {
+    return this.up.has({ mouse: button });
+  }
+
+  isMousePressed(button) {
+    return this.pressed.has({ mouse: button });
+  }
+
+  /**
+   * @param {KeyboardEvent} evt
+   */
+  onKeyDown(evt) {
+    this.up.remove({ key: evt.key });
+    this.down.add({ key: evt.key });
+    this.pressed.add({ key: evt.key });
+  }
+
+  /**
+   * @param {KeyboardEvent} evt
+   */
+  onKeyUp(evt) {
+    this.down.remove({ key: evt.key });
+    this.pressed.remove({ key: evt.key });
+    this.up.add({ key: evt.key });
+  }
+
+  /**
+   * @param {MouseEvent} evt
+   */
+  onMouseDown(evt) {
+    this.down.add({ mouse: evt.button });
+    this.pressed.add({ mouse: evt.button });
+    this.up.remove({ mouse: evt.button });
+  }
+
+  /**
+   * @param {MouseEvent} evt
+   */
+  onMouseUp(evt) {
+    this.down.remove({ mouse: evt.button });
+    this.pressed.remove({ mouse: evt.button });
+    this.up.add({ mouse: evt.button });
+  }
+
+  /**
+   * @param {MouseEvent} evt
+   */
+  onMouseMove(evt) {
+    this.mouseX = evt.clientX;
+    this.mouseY = evt.clientY;
+  }
+
+  nextFrame() {
+    this.up.clear();
+    this.down.clear();
+  }
+}
 
 class LerpManager {
   /**
@@ -61,8 +166,9 @@ class LerpManager {
 /**
  * @param {Minigame} game
  * @param {CanvasRenderingContext2D} ctx
+ * @param {InputManager} inp
  */
-function run(game, ctx) {
+function run(game, ctx, inp) {
   game.setup(ctx);
   let i = 0;
   let handler = null;
@@ -78,7 +184,7 @@ function run(game, ctx) {
     ctx.fillStyle = "white";
     ctx.fillRect(0, 0, WIDTH, HEIGHT);
 
-    game.loop(ctx, i, mgr);
+    game.loop(ctx, i, mgr, inp);
     mgr.loop();
   }, 1000.0 / FPS);
 }
@@ -95,8 +201,9 @@ class Minigame {
    * @param {CanvasRenderingContext2D} ctx
    * @param {Number} i
    * @param {LerpManager} mgr
+   * @param {InputManager} inp
    */
-  loop(ctx, i, mgr) {
+  loop(ctx, i, mgr, inp) {
     throw new Error("unimplemented");
   }
 
@@ -128,8 +235,9 @@ class MeowMinigame extends Minigame {
    * @param {CanvasRenderingContext2D} ctx
    * @param {Number} i
    * @param {LerpManager} mgr
+   * @param {InputManager} inp
    */
-  loop(ctx, i, mgr) {
+  loop(ctx, i, mgr, inp) {
     if (i % 20 == 0) {
       for (let j = 0; j < 20; j++) {
         let x = Math.random() * WIDTH;
@@ -142,10 +250,9 @@ class MeowMinigame extends Minigame {
           ctx.fillText("meow", x, y);
           x += 4 * (Math.random() - 0.5);
           y += 4 * (Math.random() - 0.5);
-          r += 10 * (Math.random() - 0.5);
-          g += 10 * (Math.random() - 0.5);
-          b += 10 * (Math.random() - 0.5);
-          console.dir(x, y, ctx);
+          r = (256 / 720) * inp.mouseX;
+          g = (256 / 720) * inp.mouseY;
+          b = (256 / 720) * inp.mouseX;
         }, 20);
       }
     }
